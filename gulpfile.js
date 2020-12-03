94bc4e44341564885c9a9dad39b5404b967fad9f
ShowPenZ <showpenz@tutanota.com>
2020-12-03 11:20:17 +0800

    feat: update

--- gulpfile.js
+++ gulpfile.js
@@ -41,34 +41,34 @@ gulp.task('minify-js', function (done) {
 });
 
 // 压缩 public/images 目录内图片(Version<3)
-// gulp.task('minify-images', function () {
-//     gulp.src('./public/images/**/*.*')
-//         .pipe(imagemin({
-//             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
-//             progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
-//             interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-//             multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化
-//         }))
-//         .pipe(gulp.dest('./public/images'));
-// });
+gulp.task('minify-images', function () {
+    gulp.src('./public/images/**/*.*')
+        .pipe(imagemin({
+            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化
+        }))
+        .pipe(gulp.dest('./public/images'));
+});
 
 // 压缩 public/images 目录内图片(Version>3)
-gulp.task('minify-images', function (done) {
-    gulp.src('./public/images/**/*.*')
-        .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.jpegtran({progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .pipe(gulp.dest('./public/images'));
-    done();
-});
+// gulp.task('minify-images', function (done) {
+//     gulp.src('./public/images/**/*.*')
+//         .pipe(imagemin([
+//             imagemin.gifsicle({interlaced: true}),
+//             imagemin.jpegtran({progressive: true}),
+//             imagemin.optipng({optimizationLevel: 5}),
+//             imagemin.svgo({
+//                 plugins: [
+//                     {removeViewBox: true},
+//                     {cleanupIDs: false}
+//                 ]
+//             })
+//         ]))
+//         .pipe(gulp.dest('./public/images'));
+//     done();
+// });
 
 //4.0以前的写法 
 //gulp.task('default', [
@@ -76,7 +76,7 @@ gulp.task('minify-images', function (done) {
 //]);
 //4.0以后的写法
 // 执行 gulp 命令时执行的任务
-gulp.task('default', gulp.series(gulp.parallel('minify-html', 'minify-css', 'minify-js', 'minify-images')), function () {
+gulp.task('default', ['minify-html', 'minify-css', 'minify-js', 'minify-images'], function () {
     console.log("----------gulp Finished----------");
     // Do something after a, b, and c are finished.
 });
